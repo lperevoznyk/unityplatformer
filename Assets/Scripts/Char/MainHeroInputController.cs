c5935dcb85124b27dc38b461e83c36dd7764d833
Leonid Perevoznyk <leonid.perevoznik@gmail.com>
2022-05-21 17:17:09 +0300

    Add basic movment for main char

---
+++ Assets/Scripts/Char/MainHeroInputController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainHeroInputController : MonoBehaviour
+{
+
+    private MainHero _mainHero;
+
+    private void Awake()
+    {
+        _mainHero = GetComponent<MainHero>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _mainHero.SetDirection(-1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _mainHero.SetDirection(1);
+        }
+        else
+        {
+            _mainHero.SetDirection(0);
+        }
+    }
+}
